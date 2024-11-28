681827cd781abe63470105fc4ebf136088ec73b0
ITheBroncoI <angelvolkenturbo15@gmail.com>
2024-11-28 01:37:05 -0600

    Característica funcional: Commit inicial Descripción breve de cambios: Commit inicial del repositorio Status: Finalizado Tiempo restante: N/A Observación: Inicialización de ramas a partir de este punto

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { HashLocationStrategy, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppLayoutModule } from './layout/app.layout.module';
+import { NotfoundComponent } from './demo/components/notfound/notfound.component';
+import { ProductService } from './demo/service/product.service';
+
+@NgModule({
+    declarations: [AppComponent, NotfoundComponent],
+    imports: [AppRoutingModule, AppLayoutModule],
+    providers: [
+        { provide: LocationStrategy, useClass: PathLocationStrategy },
+         ProductService
+    ],
+    bootstrap: [AppComponent],
+})
+export class AppModule {}
