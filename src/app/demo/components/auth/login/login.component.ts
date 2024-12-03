d4c7acf8851dd52b38cc2cb038232e7afe29eec1
habacucsoto <josehabacuc182020@gmail.com>
2024-12-02 23:15:16 -0600

    **Descripción:**
    Se añadió la funcionalidad de inicio de sesión al componente `Login`, incluyendo:
    - Uso del método `iniciarSesion` de `UserDatasourceImpl`.
    - Inyección de dependencias necesarias (`HttpClient`, `LocalStorageService`).
    - Manejo básico de errores y retroalimentación al usuario mediante mensajes en pantalla.
    - Redirección al dashboard en caso de éxito.

--- src/app/demo/components/auth/login/login.component.ts
+++ src/app/demo/components/auth/login/login.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { LayoutService } from 'src/app/layout/service/app.layout.service';
 import { Router } from "@angular/router";
+import { UserDatasourceImpl } from 'src/app/demo/service/user/datasource/user.datasource.impl';
+import { UserModel } from 'src/app/demo/service/user/model/user.model';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { LocalStorageService } from 'src/app/demo/service/localStorage/localStorageService';
+import { BadCredentialsException, BadRequestException, InternalServerException } from 'src/app/demo/exceptions/exception';
 
 @Component({
     selector: 'app-login',
@@ -17,15 +22,35 @@ import { Router } from "@angular/router";
 export class LoginComponent {
 
     valCheck: string[] = ['remember'];
-
     password!: string;
     email!: string;
+    errorMessage: string = '';
+    private userDatasource: UserDatasourceImpl;
 
     constructor(
         public layoutService: LayoutService,
-        private  router: Router,) { }
+        private  router: Router,
+        private http: HttpClient
+    ) { 
+        this.userDatasource = new UserDatasourceImpl(new LocalStorageService(), this.http);
+    }
 
-    onLogin() {
-        this.router.navigate(['/dashboard']);
+    async onLogin() {
+        const user: UserModel = {
+            email: this.email,
+            password: this.password
+        };
+
+        try {
+            const success = await this.userDatasource.iniciarSesion(user);
+            if (success) this.router.navigate(['/dashboard']);
+        } catch (error) {
+            // Incluir lógica para toast notification
+            if (error instanceof HttpErrorResponse) {
+                if (error.status === 401) throw new BadCredentialsException
+                if (error.status === 400) throw new BadRequestException();
+                if (error.status === 500) throw new InternalServerException();
+            }
+        }
     }
 }
